b5dd277b5e801113352399b5ee447a3c76bc9f18
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2024-07-27 11:46:16 +0530

    Added user profile model

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+#One to One realtionship between profile and user
 
-# Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE) # one-way cascade, profile deleted => all post deleted
+	image = models.ImageField(default= 'default.jpg', upload_to = 'profile_pics')
+
+	def __str__(self):
+		return f"{self.user.username} Profile"
+
+# make migrations to the database
