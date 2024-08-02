d62ec6df1c55b38b2177df57e96ad7159a763cdc
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2024-08-02 11:03:57 +0530

    Added models to update user profile

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 #One to One realtionship between profile and user
 
 
@@ -9,5 +10,15 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f"{self.user.username} Profile"
+
+	def save(self): # to add some fucntionality
+		super().save()
+
+		img = Image.open(self.image.path)
+		if img.height > 500 or img.width > 500 :
+			output_size = (500,500)
+			img.thumbnail(output_size, Image.LANCZOS) # resizing the image ; filter for smoothing the reduced file
+			img.save(self.image.path)
+
 
 # make migrations to the database
