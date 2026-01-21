9620ab86ace9e27fbb5b0f907c7ace41f9ee08fc
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2026-01-21 17:04:02 +0530

    Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'First Post',
+        'content': 'This is the content of the first post.',
+        'date_posted': 'April 20, 2024'
+    },
+    {
+        'author': 'Jane Smith',
+        'title': 'Second Post',
+        'content': 'This is the content of the second post.',
+        'date_posted': 'April 21, 2024'
+    }
+    
+]
+
 def home(request):
-    return HttpResponse("Welcome to the Blog Home Page!")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("This is the About Page of the Blog.")
+    return render(request, 'blog/about.html', {'title': 'About'})
